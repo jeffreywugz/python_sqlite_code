0cd3002e404eb15a382d4bfd34290480bccec9b7
Shank <shashanksharma21@gmail.com>
2014-02-14 03:42:16 +0530

    removed .DS_Store file to clean the commit

--- code/create_new_db.py
+++ code/create_new_db.py
@@ -6,19 +6,19 @@ import sqlite3
 sqlite_file = ''
 table_name = ''
 new_field = 'my_1st_column'
-field_type = INTEGER
+field_type = 'INTEGER'
 
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 # Creating a new SQLite table with 1 column 
-c.execute('CREATE TABLE {dn} ({nf} {ft})'\
-        .format(tn=table_name nf=new_field, ft=field_type))
+c.execute('CREATE TABLE {tn} ({nf} {ft})'\
+        .format(tn=table_name, nf=new_field, ft=field_type))
 
 # Creating a new SQLite table with 1 column and set it as PRIMARY KEY
 # note that PRIMARY KEY column must consist of unique values!
 c.execute('CREATE TABLE {dn} ({nf} {ft} PRIMARY KEY)'\
-        .format(tn=table_name nf=new_field, ft=field_type))
+        .format(tn=table_name, nf=new_field, ft=field_type))
 
 conn.commit()
 conn.close()
