fb903e984044115af4d0e0ba218011cccaf77752
rasbt <se.raschka@me.com>
2014-03-05 12:35:29 -0500

    ready to use create_new_db.py

--- code/add_new_column.py
+++ code/add_new_column.py
@@ -3,22 +3,25 @@
 
 import sqlite3
 
-sqlite_file = ''
-table_name = ''
-new_column = ''  # name of the new column
-column_type = '' # E.g., INTEGER, TEXT, NULL, REAL, BLOB
-default_val = '' # E.g., 1
+sqlite_file = 'my_first_db.sqlite'    # name of the sqlite database file
+table_name = 'my_table_1'	# name of the table to be created
+new_column1 = 'my_2nd_column'  # name of the new column
+new_column2 = 'my_3nd_column'  # name of the new column
+column_type = 'TEXT' # E.g., INTEGER, TEXT, NULL, REAL, BLOB
+default_val = 'Hello World' # a default value for the new column rows
 
+# Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 # A) Adding a new column without a row value
-c.execute("ALTER TABLE {dn} ADD COLUMN '{cn}' {ft}"\
-        .format(tn=table_name, cn=new_column, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column1, ct=column_type))
 
 # B) Adding a new column with a default row value
-c.execute("ALTER TABLE {tn} ADD COLUMN '{fn}' {ft} DEFAULT {df}"\
-        .format(tn=table_name, cn=new_column, ct=column_type, df=default_val))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
+        .format(tn=table_name, cn=new_column2, ct=column_type, df=default_val))
 
+# Committing changes and closing the connection to the database file
 conn.commit()
 conn.close()
