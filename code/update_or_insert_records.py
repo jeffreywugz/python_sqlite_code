fe2da7fda1f5ea0c67c00bac14fbc0f21f2f46f0
rasbt <se.raschka@me.com>
2014-03-05 12:48:48 -0500

    ready to use create_new_db.py

--- code/update_or_insert_records.py
+++ code/update_or_insert_records.py
@@ -5,22 +5,23 @@
 
 import sqlite3
 
-sqlite_file = ''
-table_name = ''
-id_column = ''
-column_name = ''
+sqlite_file = 'my_first_db.sqlite'
+table_name = 'my_table_1'
+id_column = 'my_1st_column'
+column_name = 'my_2nd_column'
 
+# Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 # Tries to insert an ID (if it does not exist yet)
 # with a specific value in a second column 
-c.execute('INSERT OR IGNORE INTO {tn} (idf, cn) VALUES (idv, val)'.\
-        format(tn=table_name, idf=id_column, cn=column_name, idv=id_value, val=value))
+c.execute("INSERT OR IGNORE INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')".\
+        format(tn=table_name, idf=id_column, cn=column_name))
 
 # Updates the newly inserted or pre-existing entry            
-c.execute('UPDATE {tn} SET {cn}={val} WHERE {idf}={idv}'.\
-        format(tn=table_name, cn=column_name, val=value, idf=id_column idv=id_value))
+c.execute("UPDATE {tn} SET {cn}=('Hi World') WHERE {idf}=(123456)".\
+        format(tn=table_name, cn=column_name, idf=id_column))
 
 conn.commit()
 conn.close()
