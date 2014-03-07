c1fb89857effd8a1f5871f3aa58c2032f76c6dcf
rasbt <se.raschka@me.com>
2014-03-07 12:26:19 -0500

    extended get_column_names

--- code/get_columnnames.py
+++ code/get_columnnames.py
@@ -3,19 +3,22 @@
 
 import sqlite3
 
-sqlite_file = ''
-table_name = ''
+sqlite_file = 'my_first_db.sqlite'
+table_name = 'my_table_3'
 
+# Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
+# Retrieve column information
+# Every column will be represented by a tuple with the following attributes:
+# (id, name, type, notnull, default_value, primary_key)
 c.execute('PRAGMA TABLE_INFO({})'.format(table_name))
 
-# every column will be repr. by a tuples with the following attributes:
-# (id, name, type, notnull, default_value, primary_key)
-
-names = [tup[1] for tup in c.fetchall()]
 # collect names in a list
-# e.g., [col1_name, col2_name, col3_name, ...]
+names = [tup[1] for tup in c.fetchall()]
+print(names)
+# e.g., ['id', 'date', 'time', 'date_time']
 
+# Closing the connection to the database file
 conn.close()
