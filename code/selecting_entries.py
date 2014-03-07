f3b5ef3d7f126372620c0ef1b2462b10117aae04
rasbt <se.raschka@me.com>
2014-03-07 01:57:11 -0500

    many syntax fixes

--- code/selecting_entries.py
+++ code/selecting_entries.py
@@ -3,31 +3,49 @@
 
 import sqlite3
 
-sqlite_file = ''
-table_name = ''
-column_1 = ''
-col_of_interest = ''
+sqlite_file = 'my_first_db.sqlite'    # name of the sqlite database file
+table_name = 'my_table_2'	# name of the table to be queried
+id_column = 'my_1st_column'
+some_id = 123456
+column_2 = 'my_2nd_column'
+column_3 = 'my_3rd_column'    
 
+# Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
-
-# All columns for row that match a certain value in column_1
-c.execute("SELECT (*) FROM {tn} WHERE {cn}=1 ".\
-        format(tn=table_name, cn=column_1)):
+# 1) Contents of all columns for row that match a certain value in 1 column
+c.execute('SELECT * FROM {tn} WHERE {cn}="Hi World"'.\
+        format(tn=table_name, cn=column_2))
 all_rows = c.fetchall()
+print('1):', all_rows)
 
-
-# A particular column for rows that match a certain value in column_1 
-c.execute("SELECT (coi) FROM {tn} WHERE {cn}=1 ".\
-        format(coi=column_of_interest, tn=table_name, cn=column_1)):
+# 2) Value of a particular column for rows that match a certain value in column_1 
+c.execute('SELECT ({coi}) FROM {tn} WHERE {cn}="Hi World"'.\
+        format(coi=column_2, tn=table_name, cn=column_2))
 all_rows = c.fetchall()
+print('2):', all_rows)
 
+# 3) Value of 2 particular columns for rows that match a certain value in 1 column
+c.execute('SELECT {coi1},{coi2} FROM {tn} WHERE {coi1}="Hi World"'.\
+        format(coi1=column_2, coi2=column_3, tn=table_name, cn=column_2))
+all_rows = c.fetchall()
+print('3):', all_rows)
 
-# Selecting only 10 rows that match a certain value in column_1
-c.execute("SELECT (*) FROM {tn} WHERE {cn}=1 LIMIT 10".\
-        format(tn=table_name, cn=column_1)):
+# 4) Selecting only up to 10 rows that match a certain value in 1 column
+c.execute('SELECT * FROM {tn} WHERE {cn}="Hi World" LIMIT 10'.\
+        format(tn=table_name, cn=column_2))
 ten_rows = c.fetchall()
+print('4):', ten_rows)
 
+# 5) Check if a certain ID exists and print its column contents
+c.execute("SELECT * FROM {tn} WHERE {idf}={my_id}".\
+        format(tn=table_name, cn=column_2, idf=id_column, my_id=some_id))
+id_exists = c.fetchone()
+if id_exists:
+    print('5): {}'.format(id_exists))
+else:
+    print('5): {} does not exist'.format(some_id))
 
+# Closing the connection to the database file
 conn.close()
--- code/update_or_insert_records.py
+++ code/update_or_insert_records.py
@@ -6,7 +6,7 @@
 import sqlite3
 
 sqlite_file = 'my_first_db.sqlite'
-table_name = 'my_table_1'
+table_name = 'my_table_2'
 id_column = 'my_1st_column'
 column_name = 'my_2nd_column'
 
@@ -14,12 +14,20 @@ column_name = 'my_2nd_column'
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
-# Tries to insert an ID (if it does not exist yet)
+
+# A) Inserts an ID with a specific value in a second column 
+try:
+    c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')".\
+        format(tn=table_name, idf=id_column, cn=column_name))
+except sqlite3.IntegrityError:
+    print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
+
+# B) Tries to insert an ID (if it does not exist yet)
 # with a specific value in a second column 
 c.execute("INSERT OR IGNORE INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')".\
         format(tn=table_name, idf=id_column, cn=column_name))
 
-# Updates the newly inserted or pre-existing entry            
+# C) Updates the newly inserted or pre-existing entry            
 c.execute("UPDATE {tn} SET {cn}=('Hi World') WHERE {idf}=(123456)".\
         format(tn=table_name, cn=column_name, idf=id_column))
 
