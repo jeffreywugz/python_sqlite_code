cc615d2322e2fe73603c10be171ece2e32ba766e
Sebastian Raschka <se.raschka@me.com>
2014-04-07 18:20:08 -0400

    Update write_from_sqlite.py

--- code/write_from_sqlite.py
+++ code/write_from_sqlite.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def write_from_query(db_name, table_name, condition, content_column, out_file):
+def write_from_query(db_name, table_name, condition, content_column, out_file, fetchmany=False):
     '''
     Writes contents from a SQLite database column to an output file
     
@@ -10,17 +10,32 @@ def write_from_query(db_name, table_name, condition, content_column, out_file):
         condition (str): Condition for querying the SQLite database table.
         content_colum (str): Name of the column that contains the content for the output file.
         out_file (str): Path of the output file that will be written.
+        fetchmany (int): Fetch multiple rows at once
 
     '''
     # Connecting to the database file
-    conn = sqlite3.connect('zinc12_drugnow_nrb(copy).sqlite')
+    conn = sqlite3.connect(db_name)
     c = conn.cursor()
 
     # Querying the database and writing the output file
-    c.execute('SELECT ({}) FROM {} WHERE {}'.format(content_column, table_name, condition))
-    with open(out_file, 'w') as outf:
-        for row in c:
-            outf.write(row[0])
+
+
+    # A) using .fetchmany(); recommended for larger databases
+    if fetchmany:
+        c.execute('SELECT ({}) FROM {} WHERE {}'.format(content_column, table_name, condition))
+        with open(out_file, 'w') as outf:
+            results = c.fetchmany(fetchmany)
+            while results:
+                for row in results:
+                    outf.write(row[0])
+                results = c.fetchmany(fetchmany)
+
+    # B) simple .execute() loop
+    else:
+        c.execute('SELECT ({}) FROM {} WHERE {}'.format(content_column, table_name, condition))
+        with open(out_file, 'w') as outf:
+            for row in c:
+                outf.write(row[0])
 
     # Closing the connection to the database
     conn.close()
